ab77ea7a49c302c5f4ef184465a45eafeffb7539
dtklumpp <dtklumpp@gmail.com>
2020-08-21 00:20:56 -0400

    Easy Going Answered

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 console.log('hello milo');
-const catName = prompt("Enter cat name");
-alert("Hello "+catName+"!");
+// const catName = prompt("Enter cat name");
+// alert("Hello "+catName+"!");
+
+for(let i = 1; i<21; i++){
+    console.log(i);
+}
 
