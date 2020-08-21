3b1818a7465556c6e8b905e8684c7e36eb118885
dtklumpp <dtklumpp@gmail.com>
2020-08-21 09:44:21 -0400

    Excited Kittens answered

--- app.js
+++ app.js
@@ -10,16 +10,18 @@ const displayArray = [];
 for(let i = 1; i<21; i++){
     displayArray.push(i);
 }
+console.log("Part 1 Solution");
 console.log(displayArray);
 
 // Get Even
 // 2. Write a for loop that will log only the even numbers in 0 through 200.
 
 
-const array2 = [];
+displayArray.splice(0, displayArray.length);
 for(let i = 0; i<=200; i+=2){
     displayArray.push(i);
 }
+console.log("Part 2 Solution");
 console.log(displayArray);
 
 
@@ -27,7 +29,36 @@ console.log(displayArray);
 // Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
 // For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
 
-console.log("Love me, pet me! HSSSSSSS!".repeat(20));
+//part1
+//console.log("Love me, pet me! HSSSSSSS!".repeat(20));
 
+//part2
+//agh gotta make a real loop now
 
+displayArray.splice(0, displayArray.length);
+dispray = displayArray; //need a shorter name for this...
+
+//outputs random integer between 0 and n
+const randInt = function (n) {
+    output = Math.floor(Math.random()*(n+1));
+    return output;
+}
+
+for(let i = 1; i<=20; i++){
+    dispray.push("Love me, pet me! HSSSSSSS!");
+    if(i%2===0){
+        //decide whether to display each string
+        x = randInt(1);
+        y = randInt(1);
+        z = randInt(1);
+        //display each string if xyz var is 1
+        if(x){dispray.push("...human...why you taking pictures of me?...");}
+        if(y){dispray.push("...the catnip made me do it...");}
+        if(z){dispray.push("...why does the red dot always get away...");}
+    }
+}
+console.log("Part 3 Solution");
+console.log(dispray);
+//hmm, this pushes a lot of ""s to display.....
+//ok now fixed
 
