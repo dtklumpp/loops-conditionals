1330566953d14939f1803fce4a1086c9181e79a5
dtklumpp <dtklumpp@gmail.com>
2020-08-21 17:08:01 -0400

    Fizz Buzz Answered

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ console.log('hello milo');
 
 const displayArray = [];
 
+const logit = function(input1){
+    console.log(input1);
+}
+
 // Easy Going
 // 1. Write a for loop that will log the numbers 1 through 20.
 
@@ -12,6 +16,11 @@ for(let i = 1; i<21; i++){
 }
 console.log("Part 1 Solution");
 console.log(displayArray);
+
+
+
+
+
 
 // Get Even
 // 2. Write a for loop that will log only the even numbers in 0 through 200.
@@ -23,6 +32,12 @@ for(let i = 0; i<=200; i+=2){
 }
 console.log("Part 2 Solution");
 console.log(displayArray);
+
+
+
+
+
+
 
 
 // // Excited Kitten
@@ -61,4 +76,29 @@ console.log("Part 3 Solution");
 console.log(dispray);
 //hmm, this pushes a lot of ""s to display.....
 //ok now fixed
+
+
+
+
+
+// FIZZ BUZZ the beginning!
+
+dispray.splice(0,dispray.length);
+
+for(let i = 1; i<=100; i++){
+    if(i%15===0){
+        dispray.push("FizzBuzz");
+    }
+    else if(i%5===0){
+        dispray.push("Buzz");
+    }
+    else if(i%3===0){
+        dispray.push("Fizz");
+    }
+    else {
+        dispray.push(i);
+    }
+}
+logit("Fizz Buzz!");
+logit(dispray);
 
